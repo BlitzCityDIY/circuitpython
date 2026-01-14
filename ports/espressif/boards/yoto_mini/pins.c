b0f20cc834089ab8ed0e3beee8165d4a02547e1c
Liz <liz@adafruit.com>
2026-01-14 16:56:35 -0500

    boot!

--- ports/espressif/boards/yoto_mini/pins.c
+++ ports/espressif/boards/yoto_mini/pins.c
@@ -67,6 +67,7 @@ static const mp_rom_map_elem_t board_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_D2), MP_ROM_PTR(&pin_GPIO2) },
 
     { MP_ROM_QSTR(MP_QSTR_D15), MP_ROM_PTR(&pin_GPIO15) },
+    { MP_ROM_QSTR(MP_QSTR_D0), MP_ROM_PTR(&pin_GPIO0) },
 
     { MP_ROM_QSTR(MP_QSTR_I2C), MP_ROM_PTR(&board_i2c_obj) },
     { MP_ROM_QSTR(MP_QSTR_UART), MP_ROM_PTR(&board_uart_obj) }
